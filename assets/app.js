3db7816e942320ecce7b2414ced1a0642aa01906
Parmentier <fparmentier@free.fr>
2021-09-21 14:11:03 +0200

    installation de fontawesome et creation de la base de données happybiztrip

--- assets/app.js
+++ assets/app.js
@@ -11,5 +11,9 @@ import './styles/app.scss'
 // Add bootstrap
 require('bootstrap')
 
+// Add Fontawesome
+require('@fortawesome/fontawesome-free/css/all.min.css')
+require('@fortawesome/fontawesome-free/js/all.js')
+
 // start the Stimulus application
 import './bootstrap'
