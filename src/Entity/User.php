702d05965be7f0188abf0f64d96bdded2f386ce7
Parmentier <fparmentier@free.fr>
2021-10-04 19:12:23 +0200

    Re-création de l'entity User avec make:user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,20 +4,13 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ORM\HasLifecycleCallbacks()
- * 
- * @UniqueEntity {
- *  fields = ("email"),
- *  message = "user.email.unique"
- * }
  */
-
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     /**
      * @ORM\Id
@@ -27,140 +20,20 @@ class User
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255, unique=true)
-     * 
-     * @Assert\NotBlank(
-     *  message = "user.email.not_blank"
-     * )
-     * 
-     * @Assert\Email(
-     *  message = "user.email.invalid"
-     * )
+     * @ORM\Column(type="string", length=180, unique=true)
      */
     private $email;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     * 
-     * @Assert\NotBlank(
-     *  message = "user.password.not_blank"
-     * )
-     * 
-     * @Assert\Length(
-     *  min=8,
-     *  minMessage = "user.password.length"
-     * )
-     */
-    private $password;
-
-    /**
      * @ORM\Column(type="json")
      */
-    private $role = [];
+    private $roles = [];
 
     /**
-     * @ORM\Column(type="string", length=1, nullable=true)
+     * @var string The hashed password
+     * @ORM\Column(type="string")
      */
-    private $gender;
-
-    /**
-     * @ORM\Column(type="string", length=100)
-     * 
-     * @Assert\NotBlank(
-     *  message = "user.firstName.not_blank"
-     * )
-     * 
-     * @Assert\Length(
-     *  min=2,
-     *  minMessage = "user.firstName.length"
-     * )
-     */
-    private $firstName;
-
-    /**
-     * @ORM\Column(type="string", length=100, nullable=true)
-     */
-    private $lastName;
-
-    /**
-     * @ORM\Column(type="date")
-     * 
-     * @Assert\NotBlank(
-     *  message = "user.birthDate.not_blank
-     * )
-     * 
-     */
-    private $birthDate;
-
-    /**
-     * @ORM\Column(type="string", length=1, nullable=true)
-     */
-    private $situation;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $avatar;
-
-    /**
-     * @ORM\Column(type="string", length=100, nullable=true)
-     */
-    private $profession;
-
-    /**
-     * @ORM\Column(type="string", length=100, nullable=true)
-     */
-    private $company;
-
-    /**
-     * @ORM\Column(type="text", nullable=true)
-     */
-    private $description;
-
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $isSubscribed;
-
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $isActive;
-
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $isDeleted;
-
-    /**
-     * @ORM\Column(type="smallint")
-     */
-    private $completed;
-
-    /**
-     * @ORM\Column(type="datetime_immutable")
-     */
-    private $createdAt;
-
-    /**
-     * @ORM\Column(type="datetime_immutable")
-     */
-    private $updatedAt;
-
-    /**
-     * @ORM\Column(type="datetime_immutable", nullable=true)
-     */
-    private $subscribPayAt;
-
-    /**
-     * @ORM\Column(type="datetime_immutable", nullable=true)
-     */
-    private $subscribBeginAt;
-
-    /**
-     * @ORM\Column(type="datetime_immutable", nullable=true)
-     */
-    private $subscribEndAt;
+    private $password;
 
     public function getId(): ?int
     {
@@ -179,7 +52,47 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -191,231 +104,23 @@ class User
         return $this;
     }
 
-    public function getRole(): ?array
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
     {
-        return $this->role;
+        return null;
     }
 
-    public function setRole(array $role): self
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
     {
-        $this->role = $role;
-
-        return $this;
-    }
-
-    public function getGender(): ?string
-    {
-        return $this->gender;
-    }
-
-    public function setGender(?string $gender): self
-    {
-        $this->gender = $gender;
-
-        return $this;
-    }
-
-    public function getFirstName(): ?string
-    {
-        return $this->firstName;
-    }
-
-    public function setFirstName(string $firstName): self
-    {
-        $this->firstName = $firstName;
-
-        return $this;
-    }
-
-    public function getLastName(): ?string
-    {
-        return $this->lastName;
-    }
-
-    public function setLastName(?string $lastName): self
-    {
-        $this->lastName = $lastName;
-
-        return $this;
-    }
-
-    public function getBirthDate(): ?\DateTimeInterface
-    {
-        return $this->birthDate;
-    }
-
-    public function setBirthDate(\DateTimeInterface $birthDate): self
-    {
-        $this->birthDate = $birthDate;
-
-        return $this;
-    }
-
-    public function getSituation(): ?string
-    {
-        return $this->situation;
-    }
-
-    public function setSituation(?string $situation): self
-    {
-        $this->situation = $situation;
-
-        return $this;
-    }
-
-    public function getAvatar(): ?string
-    {
-        return $this->avatar;
-    }
-
-    public function setAvatar(string $avatar): self
-    {
-        $this->avatar = $avatar;
-
-        return $this;
-    }
-
-    public function getProfession(): ?string
-    {
-        return $this->profession;
-    }
-
-    public function setProfession(?string $profession): self
-    {
-        $this->profession = $profession;
-
-        return $this;
-    }
-
-    public function getCompany(): ?string
-    {
-        return $this->company;
-    }
-
-    public function setCompany(?string $company): self
-    {
-        $this->company = $company;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(?string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getIsSubscribed(): ?bool
-    {
-        return $this->isSubscribed;
-    }
-
-    public function setIsSubscribed(bool $isSubscribed): self
-    {
-        $this->isSubscribed = $isSubscribed;
-
-        return $this;
-    }
-
-    public function getIsActive(): ?bool
-    {
-        return $this->isActive;
-    }
-
-    public function setIsActive(bool $isActive): self
-    {
-        $this->isActive = $isActive;
-
-        return $this;
-    }
-
-    public function getIsDeleted(): ?bool
-    {
-        return $this->isDeleted;
-    }
-
-    public function setIsDeleted(bool $isDeleted): self
-    {
-        $this->isDeleted = $isDeleted;
-
-        return $this;
-    }
-
-    public function getCompleted(): ?int
-    {
-        return $this->completed;
-    }
-
-    public function setCompleted(int $completed): self
-    {
-        $this->completed = $completed;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeImmutable $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeImmutable
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
-
-    public function getSubscribPayAt(): ?\DateTimeImmutable
-    {
-        return $this->subscribPayAt;
-    }
-
-    public function setSubscribPayAt(?\DateTimeImmutable $subscribPayAt): self
-    {
-        $this->subscribPayAt = $subscribPayAt;
-
-        return $this;
-    }
-
-    public function getSubscribBeginAt(): ?\DateTimeImmutable
-    {
-        return $this->subscribBeginAt;
-    }
-
-    public function setSubscribBeginAt(?\DateTimeImmutable $subscribBeginAt): self
-    {
-        $this->subscribBeginAt = $subscribBeginAt;
-
-        return $this;
-    }
-
-    public function getSubscribEndAt(): ?\DateTimeImmutable
-    {
-        return $this->subscribEndAt;
-    }
-
-    public function setSubscribEndAt(?\DateTimeImmutable $subscribEndAt): self
-    {
-        $this->subscribEndAt = $subscribEndAt;
-
-        return $this;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
