e739fc6c637d1c103a6203d13d87dd5c7ae4765e
Frederic Parmentier <fparmentier@free.fr>
2021-11-04 10:40:21 +0100

    changement du header de la page d'édition de profile

--- assets/js/editProfile.js
+++ assets/js/editProfile.js
@@ -1,6 +1,6 @@
 // ***** Handle interest choices *****
 // initialize value of edit_profile_listInterest
-window.addEventListener('load', () => {
+window.addEventListener('DOMContentLoaded', () => {
   var arrayListInterest = []
   document.querySelectorAll('.control-interest').forEach(interest => {
     if (interest.checked) {
@@ -12,98 +12,126 @@ window.addEventListener('load', () => {
   })
   // set to input hidden 'edit_profile_ListInterest' values of arrayListInterest
   document.getElementById('edit_profile_listInterest').value = arrayListInterest.join(';')
-})
 
-// retrieve all interests and listen on click
-document.querySelectorAll('.control-interest').forEach(interest =>
-  interest.addEventListener('click', () => {
-    var listInterest = document.getElementById('edit_profile_listInterest').value
-    // retrieve the id of interest
-    var strInterest = interest.getAttribute('id')
-    var interestId = strInterest.substring(strInterest.indexOf('_', 1) + 1, strInterest.length)
-    // action following checked or not
-    if (interest.checked) {
-      listInterest = updateListInterest(listInterest, interestId, 'A')
-    } else {
-      listInterest = updateListInterest(listInterest, interestId, 'R')
+  // retrieve all interests and listen on click
+  document.querySelectorAll('.control-interest').forEach(interest =>
+    interest.addEventListener('click', () => {
+      var listInterest = document.getElementById('edit_profile_listInterest').value
+      // retrieve the id of interest
+      var strInterest = interest.getAttribute('id')
+      var interestId = strInterest.substring(strInterest.indexOf('_', 1) + 1, strInterest.length)
+      // action following checked or not
+      if (interest.checked) {
+        listInterest = updateListInterest(listInterest, interestId, 'A')
+      } else {
+        listInterest = updateListInterest(listInterest, interestId, 'R')
+      }
+      document.getElementById('edit_profile_listInterest').value = listInterest
+    })
+  )
+  // ***** end of Handle interest choices *****
+  // ***** Handle of avatar *****
+  // Define variables
+  const defaultAvatar = 'defaultAvatar.png'
+  const fileInput = document.getElementById('uploadFile')
+  var fileAvatar
+  var cropBoxData
+  var canvasData
+  var cropper
+  // handle when show and hide modal
+  // On show : atrribute image in modal body
+  // on hide : empty the src of modal image and destroy cropper if exists
+  $('#avatarProfileModal').on('shown.bs.modal', () => {
+    document.getElementById('imgModalAvatar').setAttribute('src', document.getElementById('imgAvatarProfile').getAttribute('src'))
+    // Activate delete button if deactivated
+    if (!isButtonActivated(document.getElementById('btnDelAvatar'))) {
+      document.getElementById('btnDelAvatar').removeAttribute('disabled')
     }
-    document.getElementById('edit_profile_listInterest').value = listInterest
+    // If image = default avatar, disable delete button and add class to resize default avatar
+    if (document.getElementById('imgModalAvatar').getAttribute('src').includes(defaultAvatar)) {
+      document.getElementById('btnDelAvatar').setAttribute('disabled', 'disabled')
+      document.getElementById('imgModalAvatar').classList.add('modal-default-avatar')
+    } else {
+      cropper = new Cropper(document.getElementById('imgModalAvatar'), {
+        // autoCropArea: 0.5,
+        aspectRatio: 1,
+        viewMode: 1,
+        ready: function () {
+          // Should set crop box data first here
+          cropper.setCropBoxData(cropBoxData).setCanvasData(canvasData)
+        }
+      })
+    }
+    // Reset input file value
+    fileInput.value = ''
+  }).on('hidden.bs.modal', () => {
+    document.getElementById('imgModalAvatar').setAttribute('src', '')
+    document.getElementById('uploadFile').value = ''
+    console.log(cropper instanceof Cropper)
+    if (cropper instanceof Cropper) {
+      cropper.destroy()
+      cropper = null
+    }
+    console.log('je ferme modal')
+    console.log(cropper instanceof Cropper)
   })
-)
-// ***** end of Handle interest choices *****
-
-// ***** Handle of avatar *****
-// Define variables
-const defaultAvatar = 'defaultAvatar.png'
-const fileInput = document.getElementById('uploadFile')
-var fileAvatar
-
-// Load image when modal window is opened
-$('#avatarProfileModal').on('show.bs.modal', () => {
-  document.getElementById('imgModalAvatar').setAttribute('src', document.getElementById('imgAvatarProfile').getAttribute('src'))
-  // Activate delete button if deactivated
-  if (!isButtonActivated(document.getElementById('btnDelAvatar'))) {
-    document.getElementById('btnDelAvatar').removeAttribute('disabled')
-  }
-  // If image = default avatar, disable delete button
-  if (document.getElementById('imgModalAvatar').getAttribute('src').includes(defaultAvatar)) {
-    document.getElementById('btnDelAvatar').setAttribute('disabled', 'disabled')
-  }
-  // Reset input file value
-  fileInput.value = ''
-})
-
-/*  Click delete button on avatar modal windows
+  //
+  /*  Click delete button on avatar modal windows
     1. Replace picture by default avatar
     2. add class 'modal-default-avatar' to reduce size of default avatar
     3. Disabled button delete
     4. reset the value of input file
-*/
-document.getElementById('btnDelAvatar').addEventListener('click', () => {
-  document.getElementById('imgModalAvatar').setAttribute('src', document.getElementById('pictures').dataset.imgdefaultavatar)
-  document.getElementById('imgModalAvatar').classList.add('modal-default-avatar')
-  document.getElementById('btnDelAvatar').setAttribute('disabled', 'disabled')
-  document.getElementById('uploadFile').value = ''
-})
+    5. Destroy cropper if exists
+  */
+  document.getElementById('btnDelAvatar').addEventListener('click', () => {
+    document.getElementById('imgModalAvatar').setAttribute('src', document.getElementById('pictures').dataset.imgdefaultavatar)
+    document.getElementById('imgModalAvatar').classList.add('modal-default-avatar')
+    document.getElementById('btnDelAvatar').setAttribute('disabled', 'disabled')
+    document.getElementById('uploadFile').value = ''
+    if (cropper instanceof Cropper) {
+      cropper.destroy()
+      cropper = null
+    }
+  })
 
-// Click on button Modify
-document.getElementById('btnChangeAvatar').addEventListener('click', () => {
-  document.getElementById('uploadFile').click()
-})
+  // Click on button Modify
+  document.getElementById('btnChangeAvatar').addEventListener('click', () => {
+    document.getElementById('uploadFile').click()
+  })
 
-// Handle change of picture
-fileInput.addEventListener('change', () => {
-  fileAvatar = fileInput.files[0]
-  if (fileAvatar) {
-    const reader = new window.FileReader()
-    reader.addEventListener('load', () => {
-      const previousImage = document.getElementById('imgModalAvatar').getAttribute('src')
-      document.getElementById('imgModalAvatar').setAttribute('src', reader.result)
-      /* If previous image is default avatar
-        Remove class modal-default-avatar and disabled option
-      */
-      if (previousImage.includes(defaultAvatar)) {
-        document.getElementById('imgModalAvatar').classList.remove('modal-default-avatar')
-        // reactivate button delete
-        document.getElementById('btnDelAvatar').removeAttribute('disabled')
-      }
-    })
-    reader.readAsDataURL(fileAvatar)
-  }
-})
+  // Handle change of picture
+  fileInput.addEventListener('change', () => {
+    fileAvatar = fileInput.files[0]
+    if (fileAvatar) {
+      const reader = new window.FileReader()
+      reader.addEventListener('load', () => {
+        const previousImage = document.getElementById('imgModalAvatar').getAttribute('src')
+        document.getElementById('imgModalAvatar').setAttribute('src', reader.result)
+        /* If previous image is default avatar
+          Remove class modal-default-avatar and disabled option
+        */
+        if (previousImage.includes(defaultAvatar)) {
+          document.getElementById('imgModalAvatar').classList.remove('modal-default-avatar')
+          // reactivate button delete
+          document.getElementById('btnDelAvatar').removeAttribute('disabled')
+        }
+      })
+      reader.readAsDataURL(fileAvatar)
+    }
+  })
+  // When click on Validate button
+  document.getElementById('btnValidateAvatar').addEventListener('click', () => {
+    console.log('je valide')
+    const imgModalAvatar = document.getElementById('imgModalAvatar').getAttribute('src')
+  })
 
-// When click on Validate button
-document.getElementById('btnValidateAvatar').addEventListener('click', () => {
-  console.log('je valide')
-  const imgModalAvatar = document.getElementById('imgModalAvatar').getAttribute('src')
-})
+  // ***** End of Handle avatar *****
 
-// ***** End of Handle avatar *****
-
-// re-position the window when the modal to delete account is closed.
-$('#deleteProfileModal').on('hidden.bs.modal', () => {
-  // position at top of the page
-  $('html,body').animate({ scrollTop: 0 }, 300)
+  // re-position the window when the modal to delete account is closed.
+  $('#deleteProfileModal').on('hidden.bs.modal', () => {
+    // position at top of the page
+    $('html,body').animate({ scrollTop: 0 }, 300)
+  })
 })
 
 /*
